62f3c3dbc55873565182a42d0663340f8a65b820
kolodani <danielvinzia@gmail.com>
2024-04-24 15:12:47 -0300

    parte 018

---
+++ 018-selectAFile/demo/src/main/java/com/example/MyFrame.java
@@ -0,0 +1,44 @@
+package com.example;
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import java.io.File;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MyFrame extends JFrame implements ActionListener {
+
+    JButton button;
+
+    public MyFrame() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(500, 500);
+        this.setLayout(new FlowLayout());
+
+        button = new JButton("Select File");
+        button.addActionListener(this);
+
+        this.add(button);
+        this.pack();
+        this.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == button) {
+            JFileChooser fileChooser = new JFileChooser();
+
+            fileChooser.setCurrentDirectory(new File("."));
+
+            // int response = fileChooser.showOpenDialog(null);
+            int response = fileChooser.showSaveDialog(null);
+
+            if (response == JFileChooser.APPROVE_OPTION) {
+                File file = new File(fileChooser.getSelectedFile().getAbsolutePath());
+                System.out.println(file);
+            }
+        }
+    }
+}
