59eda3b78dc2b153f34973f45bf3c44b380ba7cb
kolodani <danielvinzia@gmail.com>
2024-05-01 21:03:28 -0300

    parte 024

---
+++ 024-2DGraphics/demo/src/main/java/com/example/MyFrame.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import javax.swing.JFrame;
+
+public class MyFrame extends JFrame {
+
+    MyPanel panel;
+
+    MyFrame() {
+
+        panel = new MyPanel();
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        this.add(panel);
+        this.pack();
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+    }
+
+}
